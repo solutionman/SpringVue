d6a42df5c98204998697dd29cef93a5ab86e7a91
lom <solutionman@inbox.ru>
2020-09-23 21:03:38 +0700

    ResponseEntity raw usage

--- backend/src/main/java/com/social/backend/controller/TestController.java
+++ backend/src/main/java/com/social/backend/controller/TestController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping(value = "homepage")
-    public ResponseEntity getHome() {
+    public ResponseEntity<?> getHome() {
         return ResponseEntity.ok("Welcome to Your home page");
     }
 
     @GetMapping(value = "onlyforadmin/welcome")
-    public ResponseEntity getSecuredAdmin() {
+    public ResponseEntity<?> getSecuredAdmin() {
         return ResponseEntity.ok("Welcome to the secured page. Only for Admin");
     }
 
     @GetMapping(value = "secured/welcome")
-    public ResponseEntity getSecured() {
+    public ResponseEntity<?> getSecured() {
         return ResponseEntity.ok("Welcome to the secured page");
     }
 
     @PostMapping(value = "secured/postdata")
-    public ResponseEntity postData() {
+    public ResponseEntity<?> postData() {
         return ResponseEntity.ok("Data is saved");
     }
 }
