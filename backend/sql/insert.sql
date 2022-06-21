29884b84779806949e9f3842d0f08269a44f5e2c
solutionman <solutionman@inbox.ru>
2022-06-21 09:46:37 +0700

    sql for user insert

--- backend/sql/insert.sql
+++ backend/sql/insert.sql
@@ -3,5 +3,8 @@ INSERT INTO roles VALUES ( 2, 'ADMIN' );
 INSERT INTO roles VALUES ( 3, 'ROLE_GUEST' );
 INSERT INTO roles VALUES ( 4, 'ROLE_PERFORMER' );
 
-INSERT INTO users VALUES (1,'admin','$2a$11$vwr8793lnCpvfD5T6/K16e0PXvTXGa0xE37Uez7WQJWoT4Wg5FN6K'); --12345678
-INSERT INTO user_roles values (1,2);
+INSERT INTO users (id, username, password) VALUES (1,'admin','$2a$11$vwr8793lnCpvfD5T6/K16e0PXvTXGa0xE37Uez7WQJWoT4Wg5FN6K'); --12345678
+INSERT INTO user_roles (user_id, role_id) values (1,2);
+
+INSERT INTO users (id, username, password) VALUES (2,'user','$2a$11$vwr8793lnCpvfD5T6/K16e0PXvTXGa0xE37Uez7WQJWoT4Wg5FN6K'); --12345678
+INSERT INTO user_roles (user_id, role_id) values (2,1);
